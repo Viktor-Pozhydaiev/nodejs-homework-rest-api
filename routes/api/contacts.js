37e4138122791e5ea26208cc1cc00f2230e8c834
Viktor <jude.peper@gmail.com>
2023-02-24 09:12:16 +0100

    fixed conflict

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,21 +2,20 @@ const express = require("express");
 
 const control = require("../../controllers/contacts");
 
-<<<<<<< HEAD
-const { validateBody, isValidId } = require("../../middlewares");
+const {validateBody,isValidId}=require("../../middlewares")
 
 const { schemas } = require("../../models/contact");
-=======
+
 const { validateBody } = require("../../middlewares");
 
 const schemas = require("../../schemas/contactsSchema");
->>>>>>> 4a1d49b758e041cb4bc30b251abfba9efe942e48
+
 
 const router = express.Router();
 
 router.get("/", control.getAll);
 
-<<<<<<< HEAD
+
 router.get("/:contactId", isValidId, control.getById);
 
 router.post("/", validateBody(schemas.addSchema), control.addContact);
@@ -37,7 +36,7 @@ router.patch(
 
 router.delete("/:contactId", isValidId, control.deletedContactById);
 
-=======
+
 router.get("/:contactId", control.getById);
 
 router.post("/", validateBody(schemas.addSchema), control.addContact);
@@ -50,5 +49,4 @@ router.put(
   control.updateContactById
 );
 
->>>>>>> 4a1d49b758e041cb4bc30b251abfba9efe942e48
 module.exports = router;
